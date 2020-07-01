b7cd9933c4871d836445c3d9cba29dab7b029630
kapilan <kapilansamaraj@gmail.com>
2020-07-01 23:21:28 +0530

    Updates

---
+++ precache-manifest.652a4bf0086aeb853b06a897922a5921.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b8a5ecf2a02cdc65396b49aacc30694",
+    "url": "/hello-world/index.html"
+  },
+  {
+    "revision": "c71e17513a93d4736758",
+    "url": "/hello-world/static/css/main.de0bd370.chunk.css"
+  },
+  {
+    "revision": "39ab8a6f4258d2024014",
+    "url": "/hello-world/static/js/2.81cff170.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello-world/static/js/2.81cff170.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c71e17513a93d4736758",
+    "url": "/hello-world/static/js/main.d7dbafdf.chunk.js"
+  },
+  {
+    "revision": "3620075c8cadf60b5829",
+    "url": "/hello-world/static/js/runtime-main.dd4d0bb0.js"
+  }
+]);
